e70b70eda628e2f216002c026680afb125c5597f
mnikn <mnikn@outlook.com>
2023-07-08 22:05:42 +0800

    feat: update level

--- src/levels/components/spike.gd
+++ src/levels/components/spike.gd
@@ -7,12 +7,19 @@ func _ready():
 	await self.get_tree().create_timer(self.delay_start).timeout
 	if self.spike_interval != 0:
 		self.start_interval()
+	else:
+		$Skin.set_animation_file("res://assets/spike-idle.json")
+		$Skin.start()
 
 
 func start_interval():
+	$Skin.set_animation_file("res://assets/spike-start.json")
+	$Skin.start()
 	$Collision.disabled = false
 	await self.get_tree().create_timer(self.spike_interval).timeout
 	$Collision.disabled = true
+	$Skin.set_animation_file("res://assets/spike-back.json")
+	$Skin.start()
 	await self.get_tree().create_timer(self.spike_interval).timeout
 	self.start_interval()
 
--- src/levels/level-manager.gd
+++ src/levels/level-manager.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const intial_scene = "res://src/levels/level-1.tscn"
+const intial_scene = "res://src/levels/level-13.tscn"
 var changing_color = false
 
 func _ready():
